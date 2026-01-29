0a7afd5b3f69d07b29b6c0e6271047bacea60ae6
Arjav Jain <arjavjain.jain1942@gmail.com>
2026-01-29 17:02:30 +0530

    feat: Implement and integrate `func_add` for integer addition, updating test messages and flow.

--- out.asm
+++ out.asm
@@ -1,10 +1,37 @@
 section .text
 global _start
+
+func_add:
+    push rbp
+    mov rbp, rsp
+    push QWORD [rbp + 16]
+    push QWORD [rbp + 24]
+    mov rax, QWORD [rsp + 8]
+    push rax
+    mov rax, QWORD [rsp + 8]
+    push rax
+    pop rbx
+    pop rax
+    add rax, rbx
+    push rax
+    pop rax
+    leave
+    ret
+    add rsp, 0
+    mov rax, 0
+    leave
+    ret
+
 _start:
     mov rax, 1
     mov rdi, 1
     lea rsi, [rel msg_0]
     mov rdx, 35
+    syscall
+    mov rax, 1
+    mov rdi, 1
+    lea rsi, [rel newline_const]
+    mov rdx, 1
     syscall
     mov rax, 1
     mov rdi, 1
@@ -322,6 +349,27 @@ label15:
     mov rax, 1
     mov rdi, 1
     lea rsi, [rel msg_21]
+    mov rdx, 22
+    syscall
+    mov rax, 20
+    push rax
+    mov rax, 10
+    push rax
+    call func_add
+    add rsp, 16
+    push rax
+    mov rax, 1
+    mov rdi, 1
+    lea rsi, [rel msg_22]
+    mov rdx, 19
+    syscall
+    mov rax, QWORD [rsp + 0]
+    push rax
+    pop rdi
+    call print_int
+    mov rax, 1
+    mov rdi, 1
+    lea rsi, [rel msg_23]
     mov rdx, 23
     syscall
     mov rax, 3
@@ -342,7 +390,7 @@ label17:
     jz label18
     mov rax, 1
     mov rdi, 1
-    lea rsi, [rel msg_22]
+    lea rsi, [rel msg_24]
     mov rdx, 8
     syscall
     mov rax, QWORD [rsp + 0]
@@ -364,22 +412,22 @@ label17:
 label18:
     mov rax, 1
     mov rdi, 1
-    lea rsi, [rel msg_23]
+    lea rsi, [rel msg_25]
     mov rdx, 14
     syscall
     mov rax, 1
     mov rdi, 1
-    lea rsi, [rel msg_24]
+    lea rsi, [rel msg_26]
     mov rdx, 24
     syscall
     mov rax, 1
     mov rdi, 1
-    lea rsi, [rel msg_25]
+    lea rsi, [rel msg_27]
     mov rdx, 58
     syscall
     mov rax, 1
     mov rdi, 1
-    lea rsi, [rel msg_26]
+    lea rsi, [rel msg_28]
     mov rdx, 31
     syscall
     mov rax, 0
@@ -389,7 +437,7 @@ label18:
     syscall
     mov rax, 1
     mov rdi, 1
-    lea rsi, [rel msg_27]
+    lea rsi, [rel msg_29]
     mov rdx, 21
     syscall
     mov rax, 60
@@ -441,31 +489,34 @@ section .bss
 buffer: resb 32
 
 section .data
-msg_27: db "THIS SHOULD NOT PRINT", 0
-msg_24: db "--- 5. Comments Test ---", 0
-msg_23: db "Loop Finished!", 0
-msg_22: db "Counter:", 0
-msg_19: db "val is 999 (Incorrect)", 0
-msg_17: db "val is 50 (Correct - ormaybe)", 0
-msg_16: db "val is 10 (Incorrect)", 0
-msg_14: db "b >= 20 (True - Correct)", 0
-msg_13: db "a <= 10 (True - Correct)", 0
-msg_1: db "--- 1. Variables and Arithmetic ---", 0
-msg_2: db "x (10):", 0
-msg_0: db "=== STARTING COMPREHENSIVE TEST ===", 0
-msg_10: db "10 > 20 (False - Incorrect)", 0
-msg_15: db "--- 3. Control Flow (maybe, ormaybe, moveon) ---", 0
-msg_3: db "y (5):", 0
-msg_12: db "a != b (True - Correct)", 0
-msg_18: db "val is else (Incorrect)", 0
-msg_4: db "Addition (x + y = 15):", 0
-msg_26: db "=== TEST COMPLETE - EXITING ===", 0
-msg_5: db "Subtraction (x - y = 5):", 0
-msg_25: db "You should not see any errors from the hidden block above.", 0
-msg_20: db "val is not 999 (Correct - moveon)", 0
-msg_6: db "Multiplication (x * y = 50):", 0
-msg_21: db "--- 4. Loops (wait) ---", 0
-msg_7: db "Division (x / y = 2):", 0
-msg_8: db "--- 2. Logic and Comparisons ---", 0
-msg_9: db "10 < 20 (True - Correct)", 0
+newline_const: db 10, 0
+msg_29: db "THIS SHOULD NOT PRINT", 0
 msg_11: db "a == 10 (True - Correct)", 0
+msg_9: db "10 < 20 (True - Correct)", 0
+msg_8: db "--- 2. Logic and Comparisons ---", 0
+msg_7: db "Division (x / y = 2):", 0
+msg_23: db "--- 4. Loops (wait) ---", 0
+msg_6: db "Multiplication (x * y = 50):", 0
+msg_20: db "val is not 999 (Correct - moveon)", 0
+msg_22: db "add(10, 20) result:", 0
+msg_27: db "You should not see any errors from the hidden block above.", 0
+msg_5: db "Subtraction (x - y = 5):", 0
+msg_28: db "=== TEST COMPLETE - EXITING ===", 0
+msg_4: db "Addition (x + y = 15):", 0
+msg_18: db "val is else (Incorrect)", 0
+msg_12: db "a != b (True - Correct)", 0
+msg_15: db "--- 3. Control Flow (maybe, ormaybe, moveon) ---", 0
+msg_10: db "10 > 20 (False - Incorrect)", 0
+msg_0: db "=== STARTING COMPREHENSIVE TEST ===", 0
+msg_2: db "x (10):", 0
+msg_1: db "--- 1. Variables and Arithmetic ---", 0
+msg_13: db "a <= 10 (True - Correct)", 0
+msg_21: db "--- 3.5. Functions ---", 0
+msg_14: db "b >= 20 (True - Correct)", 0
+msg_16: db "val is 10 (Incorrect)", 0
+msg_3: db "y (5):", 0
+msg_17: db "val is 50 (Correct - ormaybe)", 0
+msg_19: db "val is 999 (Incorrect)", 0
+msg_24: db "Counter:", 0
+msg_25: db "Loop Finished!", 0
+msg_26: db "--- 5. Comments Test ---", 0
